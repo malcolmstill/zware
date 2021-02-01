e1db1d5c8f03a8385e2c9ab6ee67415ad7d93ff6
Malcolm Still <malcolm.still@gmail.com>
2021-02-01 03:46:15 +0000

    Working reading of generic sections

--- src/engine.zig
+++ src/engine.zig
@@ -14,22 +14,17 @@ pub const Engine = struct {
     pub fn loadModule(self: *Self, module: []const u8) !void {
         var buffer = Format.init(module);
         var header = try buffer.readModule();
+        std.log.info("header: {}\n", .{header});
+
+        var first_section = try buffer.readSection();
+        std.log.info("section[0]: {}\n", .{first_section});
+
+        var second_section = try buffer.readSection();
+        std.log.info("section[1]: {}\n", .{second_section});
+
+        var third_section = try buffer.readSection();
+        std.log.info("section[2]: {}\n", .{third_section});
     }
 
     pub fn getFunction(self: *Self, function_name: []const u8) usize {}
-};
-
-const Sections = enum(u8) {
-    Custom,
-    Type,
-    Import,
-    Function,
-    Table,
-    Memory,
-    Global,
-    Export,
-    Start,
-    Element,
-    Code,
-    Data,
 };
--- src/format.zig
+++ src/format.zig
@@ -1,28 +1,69 @@
 const std = @import("std");
 const mem = std.mem;
+const leb = std.debug.leb;
 
 pub const Format = struct {
     module: []const u8 = undefined,
-    offset: usize = 0,
+    offset: []const u8 = undefined,
 
     pub fn init(module: []const u8) Format {
         return Format{
             .module = module,
+            .offset = module,
         };
     }
 
     pub fn readModule(self: *Format) !Module {
-        if (!mem.eql(u8, self.module[self.offset .. self.offset + 4], "\x00asm")) return error.MagicNumberNotFound;
-        self.offset += 4;
-        const version = mem.readInt(u32, @ptrCast(*const [@sizeOf(u32)]u8, &self.module[self.offset]), .Little);
-        std.log.info("version: {}\n", .{version});
+        if (!mem.eql(u8, self.module[0..4], "\x00asm")) return error.MagicNumberNotFound;
+        self.offset = self.offset[4..];
+
+        const version = mem.readInt(u32, @ptrCast(*const [@sizeOf(u32)]u8, &self.offset[0]), .Little);
+        self.offset = self.offset[4..];
 
         return Module{
             .version = version,
         };
     }
+
+    pub fn readSection(self: *Format) !Section {
+        const id: SectionType = @intToEnum(SectionType, self.offset[0]);
+        self.offset = self.offset[1..];
+
+        var reader = std.io.fixedBufferStream(self.offset);
+        const size = try leb.readULEB128(u32, reader.reader());
+        self.offset = self.offset[4..];
+
+        defer self.offset = self.offset[size..];
+
+        return Section{
+            .id = id,
+            .size = size,
+            .contents = self.offset[0..size],
+        };
+    }
 };
 
-const Module = packed struct {
+const Module = struct {
     version: u32,
+};
+
+const Section = struct {
+    id: SectionType,
+    size: u32,
+    contents: []const u8,
+};
+
+const SectionType = enum(u8) {
+    Custom,
+    Type,
+    Import,
+    Function,
+    Table,
+    Memory,
+    Global,
+    Export,
+    Start,
+    Element,
+    Code,
+    Data,
 };
