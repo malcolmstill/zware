3f10fcbd8187823f7837ca3edc68922f2ef26897
Malcolm Still <malcolm.still@gmail.com>
2021-02-01 22:23:45 +0000

    WIP instructions

--- src/format.zig
+++ src/format.zig
@@ -33,7 +33,7 @@ pub const Format = struct {
             .functions = Functions.init(self.alloc),
             .tables = Tables.init(self.alloc),
             .memories = Memories.init(self.alloc),
-            // .globals = Globals.init(self.alloc),
+            .globals = Globals.init(self.alloc),
             // .exports = Exports.init(self.alloc),
             // .starts = Starts.init(self.alloc),
             // .elements = Elements.init(self.alloc),
@@ -49,9 +49,6 @@ pub const Format = struct {
 
         const size = try leb.readULEB128(u32, rd);
         std.debug.warn("section bytes: {}, next byte {}\n", .{ size, self.module[rd.context.pos] });
-
-        const offset = rd.context.pos;
-        // const section_data = self.module[offset .. offset + size];
 
         switch (id) {
             .Type => {
@@ -73,6 +70,10 @@ pub const Format = struct {
             .Memory => {
                 const count = try self.readMemorySection(module);
                 std.debug.warn("read {} Memory\n", .{count});
+            },
+            .Global => {
+                const count = try self.readGlobalSection(module);
+                std.debug.warn("read {} Global\n", .{count});
             },
             else => {},
         }
@@ -232,6 +233,33 @@ pub const Format = struct {
 
         return count;
     }
+
+    fn readGlobalSection(self: *Format, module: *Module) !usize {
+        const rd = self.buf.reader();
+        const count = try leb.readULEB128(u32, rd);
+
+        var i: usize = 0;
+        while (i < count) : (i += 1) {
+            const global_type = try rd.readEnum(ValueType, .Little);
+            const mutability = try rd.readEnum(Mutability, .Little);
+            const offset = rd.context.pos;
+
+            var j: usize = 0;
+            while (true) : (j += 1) {
+                const byte = try rd.readEnum(Instructions, .Little);
+                if (byte == Instructions.End) break;
+            }
+            const code = self.module[offset .. offset + j];
+
+            try module.globals.append(Global{
+                .value_type = global_type,
+                .mutability = mutability,
+                .code = code,
+            });
+        }
+
+        return count;
+    }
 };
 
 const Module = struct {
@@ -243,7 +271,7 @@ const Module = struct {
     functions: Functions,
     tables: Tables,
     memories: Memories,
-    // globals: Globals,
+    globals: Globals,
     // exports: Exports,
     // starts: Starts,
     // elements: Elements,
@@ -265,6 +293,11 @@ const LimitType = enum(u8) {
 const Limit = struct {
     min: u32,
     max: u32,
+};
+
+const Mutability = enum(u8) {
+    Immutable,
+    Mutable,
 };
 
 const SectionType = enum(u8) {
@@ -297,7 +330,7 @@ const Imports = ArrayList(Import);
 const Functions = ArrayList(u32); // We can't use view into data because values are leb
 const Tables = ArrayList(Limit);
 const Memories = ArrayList(Limit);
-// const Globals = ArrayList(Global);
+const Globals = ArrayList(Global);
 // const Exports = ArrayList(Export);
 // const Starts = ArrayList(Start);
 // const Codes = ArrayList(Code);
@@ -310,6 +343,98 @@ const FuncType = struct {
     results_count: usize,
 };
 
+const Global = struct {
+    value_type: ValueType,
+    mutability: Mutability,
+    code: []const u8,
+};
+
 const Import = struct {
     module: []const u8, name: []const u8, desc_tag: u8, desc: u8
+};
+
+const Instructions = enum(u8) {
+    Unreachable = 0x0,
+    Nop = 0x01,
+    Block = 0x02, // bt
+    Loop = 0x03, // bt
+    If = 0x04, // bt
+    Else = 0x05,
+    End = 0x0b,
+    Br = 0x0c,
+    BrIf = 0x0d,
+    BrTable = 0x0e,
+    Return = 0x0f,
+    Call = 0x10,
+    CallIndirect = 0x11,
+    Drop = 0x1a,
+    Select = 0x1b,
+    LocalGet = 0x20,
+    LocalSet = 0x21,
+    LocalTee = 0x22,
+    GlobalGet = 0x23,
+    GlobalSet = 0x24,
+    I32Load = 0x28,
+    I64Load = 0x29,
+    F32Load = 0x2a,
+    F64Load = 0x2b,
+    I32Load8S = 0x2c,
+    I32Load8U = 0x2d,
+    I32Load16S = 0x2e,
+    I32Load16U = 0x2f,
+    I64Load8S = 0x30,
+    I64Load8U = 0x31,
+    I64Load16S = 0x32,
+    I64Load16U = 0x33,
+    I64Load32S = 0x34,
+    I64Load32U = 0x35,
+    I32Store = 0x36,
+    I64Store = 0x37,
+    F32Store = 0x38,
+    F64Store = 0x39,
+    I32Store8 = 0x3a,
+    I32Store16 = 0x3b,
+    I64Store8 = 0x3c,
+    I64Store16 = 0x3d,
+    I64Store32 = 0x3e,
+    MemorySize = 0x3f,
+    MemoryGrow = 0x40,
+    I32Const = 0x41,
+    I64Const = 0x42,
+    F32Const = 0x43,
+    F64Const = 0x44,
+    I32Eqz = 0x45,
+    I32Eq = 0x46,
+    I32Ne = 0x47,
+    I32LtS = 0x48,
+    I32LtU = 0x49,
+    I32GtS = 0x4a,
+    I32GtU = 0x4b,
+    I32LeS = 0x4c,
+    I32LeU = 0x4d,
+    I32GeS = 0x4e,
+    I32GeU = 0x4f,
+    I64Eqz = 0x50,
+    I64Eq = 0x51,
+    I64Ne = 0x52,
+    I64LtS = 0x53,
+    I64LtU = 0x54,
+    I64GtS = 0x55,
+    I64GtU = 0x56,
+    I64LeS = 0x57,
+    I64LeU = 0x58,
+    I64GeS = 0x59,
+    I64GeU = 0x5a,
+    F32Eq = 0x5b,
+    F32Ne = 0x5c,
+    F32Lt = 0x5d,
+    F32Gt = 0x5e,
+    F32Le = 0x5f,
+    F32Ge = 0x60,
+    F64Eq = 0x61,
+    F64Ne = 0x62,
+    F64Lt = 0x63,
+    F64Gt = 0x64,
+    F64Le = 0x65,
+    F64Ge = 0x66,
 };
