6d64d6d1f195720198b7d1025decc90ccc1ec15d
Malcolm Still <malcolm.still@gmail.com>
2021-02-12 21:19:06 +0000

    module.zig: add break-bare block test

--- src/module.zig
+++ src/module.zig
@@ -695,4 +695,6 @@ test "block test" {
     testing.expectEqual(@as(i32, 12), try modinst.invoke("as-binary-operands", .{}, i32, .{}));
     testing.expectEqual(@as(i32, 0), try modinst.invoke("as-compare-operands", .{}, i32, .{}));
     testing.expectEqual(@as(i32, 27), try modinst.invoke("as-mixed-operands", .{}, i32, .{}));
+
+    testing.expectEqual(@as(i32, 19), try modinst.invoke("break-bare", .{}, i32, .{}));
 }
