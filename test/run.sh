cdd82eea2fcdc9576b71011c50f3e8b589715648
Malcolm Still <malcolm.still@gmail.com>
2021-02-13 23:39:38 +0000

    test/run.sh: i32

--- test/run.sh
+++ test/run.sh
@@ -8,5 +8,7 @@ function cleanup {
 
 trap cleanup EXIT
 
+wast2json test/testsuite/i32.wast || exit 1
+bin/testrunner i32.json || exit 1
 wast2json test/testsuite/block.wast || exit 1
 bin/testrunner block.json || exit 1
