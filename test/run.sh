04f116cc339a7965bf07aef9bbe6af47e9b86980
Malcolm Still <malcolm.still@gmail.com>
2021-02-13 23:34:42 +0000

    test/run.sh: clean up even on exit 1

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+function cleanup {
+    rm *.json
+    rm *.wat
+    rm *.wasm
+}
+
+trap cleanup EXIT
+
 wast2json test/testsuite/block.wast || exit 1
 bin/testrunner block.json || exit 1
-rm *.json
-rm *.wat
-rm *.wasm
