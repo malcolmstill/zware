c62dbf42ce3dcdd46973c64ced6d7f8a85c8721d
Malcolm Still <malcolm.still@gmail.com>
2021-02-13 23:27:58 +0000

    test/run.sh: exit on failure

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wast2json test/testsuite/block.wast
-bin/testrunner block.json
+wast2json test/testsuite/block.wast || exit 1
+bin/testrunner block.json || exit 1
 rm *.json
 rm *.wat
 rm *.wasm
