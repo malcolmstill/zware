56a6838cac59a43d03b8bccf2e0471f07573f97b
Malcolm Still <malcolm.still@gmail.com>
2021-02-13 17:09:56 +0000

    test/src/main.zig: add json to the point that zig 0.7.1 will complain
    - Now to pull down zig master and use the new union support for json
      parsing

--- test/src/main.zig
+++ test/src/main.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
 const fs = std.fs;
 const process = std.process;
-const GeneralPurposeAllocator = std.heap.GeneralPurposeAllocator;
-const ArenaAllocator = std.heap.ArenaAllocator;
+const json = std.json;
 const foxwren = @import("foxwren");
 const Module = foxwren.Module;
 const Store = foxwren.Store;
+const GeneralPurposeAllocator = std.heap.GeneralPurposeAllocator;
+const ArenaAllocator = std.heap.ArenaAllocator;
 
 // testrunner
 //
@@ -24,7 +25,7 @@ pub fn main() anyerror!void {
 
     defer _ = gpa.deinit();
 
-    // 1. Get wasm file to test from command line
+    // 1. Get .json file from command line
     var args = process.args();
     _ = args.skip();
     const filename = args.nextPosix() orelse return error.NoFilename;
@@ -32,9 +33,17 @@ pub fn main() anyerror!void {
     var arena = ArenaAllocator.init(&gpa.allocator);
     defer _ = arena.deinit();
 
-    const program = try fs.cwd().readFileAlloc(&arena.allocator, filename, 0xFFFFFFF);
+    // 2. Parse json and find .wasm file
+    const json_string = try fs.cwd().readFileAlloc(&arena.allocator, filename, 0xFFFFFFF);
+    std.debug.warn("json_string = {}\n", .{json_string});
+    const r = try json.parse(Wast, &json.TokenStream.init(json_string), json.ParseOptions{ .allocator = &arena.allocator });
+    std.debug.warn("r = {}\n", .{r});
+    const wasm = "test.wasm";
 
-    // 2. Initialise our module
+    // 3. Load .wasm from file
+    const program = try fs.cwd().readFileAlloc(&arena.allocator, wasm, 0xFFFFFFF);
+
+    // 4. Initialise our module
     var module = Module.init(&arena.allocator, program);
     try module.decode();
 
@@ -44,5 +53,49 @@ pub fn main() anyerror!void {
 
     var modinst = try module.instantiate(&store);
 
-    // 3. Run test
+    // 5. Loop over assert_return's and assert_trap's
 }
+
+const Wast = struct {
+    source_filename: []const u8,
+    commands: []const Command,
+};
+
+const Command = union(enum) {
+    module: struct {
+        comptime @"type": []const u8 = "module",
+        line: []const u8,
+        filename: []const u8,
+    },
+    assert_return: struct {
+        comptime @"type": []const u8 = "assert_return",
+        line: []const u8,
+        action: Action,
+        expected: []const Value,
+    },
+    assert_malformed: struct {
+        comptime @"type": []const u8 = "assert_malformed",
+        line: []const u8,
+        filename: []const u8,
+        text: []const u8,
+        module_type: []const u8,
+    },
+    assert_invalid: struct {
+        comptime @"type": []const u8 = "assert_invalid",
+        line: []const u8,
+        filename: []const u8,
+        text: []const u8,
+        module_type: []const u8,
+    },
+};
+
+const Action = struct {
+    comptime @"type": []const u8 = "invoke",
+    field: []const u8,
+    args: []const Value,
+};
+
+const Value = struct {
+    @"type": []const u8,
+    value: []const u8,
+};
