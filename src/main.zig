97d3c0be8c0c1e6191143174a0c145f11647090e
Malcolm Still <malcolm.still@gmail.com>
2021-02-14 01:51:26 +0000

    main.zig: expose Memory and ModuleInstance

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,7 @@
 pub const Module = @import("module.zig").Module;
+pub const ModuleInstance = @import("module.zig").ModuleInstance;
 pub const Store = @import("interpreter/store.zig").Store;
+pub const Memory = @import("interpreter/store.zig").Memory;
 
 test "" {
     _ = @import("validator.zig");
