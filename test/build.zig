227796bd293dc6b633c5a57da357e2b49c514026
Malcolm Still <malcolm.still@gmail.com>
2021-02-15 00:02:36 +0000

    testrunner.zig: main.zig -> testrunner.zig

--- test/build.zig
+++ test/build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("testrunner", "src/main.zig");
+    const exe = b.addExecutable("testrunner", "src/testrunner.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.addPackagePath("foxwren", "../src/main.zig");
