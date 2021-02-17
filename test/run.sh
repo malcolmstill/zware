8433ee5e43a981d39db74cdcc0c3a37eee04eb1f
Malcolm Still <malcolm.still@gmail.com>
2021-02-17 20:04:03 +0000

    test: endianness.wast (#57)

--- test/run.sh
+++ test/run.sh
@@ -44,8 +44,8 @@ bin/testrunner const.json || exit 1
 wast2json test/testsuite/conversions.wast || exit 1
 bin/testrunner conversions.json || exit 1
 
-# wast2json test/testsuite/endianness.wast || exit 1
-# bin/testrunner endianness.json || exit 1
+wast2json test/testsuite/endianness.wast || exit 1
+bin/testrunner endianness.json || exit 1
 
 wast2json test/testsuite/f32_bitwise.wast || exit 1
 bin/testrunner f32_bitwise.json || exit 1
