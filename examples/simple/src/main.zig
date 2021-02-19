42ef3eea4f9fc24ff7e60407f96a10560c5aa6b2
Malcolm Still <malcolm.still@gmail.com>
2021-02-19 21:48:59 +0000

    style: remove comments

--- examples/simple/src/main.zig
+++ examples/simple/src/main.zig
@@ -14,16 +14,13 @@ var gpa = GeneralPurposeAllocator(.{}){};
 pub fn main() anyerror!void {
     defer _ = gpa.deinit();
 
-    // We will allocate the module with an arena allocator and free it all once
     var arena = ArenaAllocator.init(&gpa.allocator);
     defer _ = arena.deinit();
 
     const program = try fs.cwd().readFileAlloc(&arena.allocator, "../../test/fib.wasm", 0xFFFFFFF);
 
-    // Load and parse a module
     var module = Module.init(&arena.allocator, program);
     try module.decode();
-    module.print();
 
     var module_instance = try module.instantiate();
 
