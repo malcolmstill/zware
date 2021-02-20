229a81170d7b6d7033cd9456feef79af0004866c
Malcolm Still <malcolm.still@gmail.com>
2021-02-20 16:24:33 +0000

    refactor: use ArrayListStore

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
 pub const Module = @import("module.zig").Module;
 pub const Instance = @import("instance.zig").Instance;
-pub const Store = @import("store.zig").Store;
+pub const Store = @import("store.zig").ArrayListStore;
 pub const Memory = @import("memory.zig").Memory;
 pub const ValueType = @import("common.zig").ValueType;
 
