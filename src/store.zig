64c929d1b1be3f200f8ddb0ea40ec0bf247367fd
Malcolm Still <malcolm.still@gmail.com>
2021-02-20 15:57:29 +0000

    refactor: rename Store to ArrayListStore
    - What I'm trying to get at here is that we might
      want different implementations of a Store such that
      the user can control how the memrories / tables / etc.
      are allocated
    - What I have in the current store is actually a store that
      is using ArrayLists to store different things. So name it
      to imply that. We might want a different store that uses
      doubly-linked lists for the resources, in a situation where
      we think we will constantly allocate and deallocate resources

--- src/store.zig
+++ src/store.zig
@@ -5,14 +5,23 @@ const ArrayList = std.ArrayList;
 const Memory = @import("memory.zig").Memory;
 const Table = @import("table.zig").Table;
 
-pub const Store = struct {
+// - Stores provide the runtime memory shared between modules
+// - For different applications you may want to use a store that
+//   that allocates individual items differently. For example,
+//   if you know ahead of time that you will only load a fixed
+//   number of modules during the lifetime of the program, then
+//   you might be quite happy with a store that uses ArrayLists
+//   with an arena allocator, i.e. you are going to deallocate
+//   everything at the same time.
+
+pub const ArrayListStore = struct {
     alloc: *mem.Allocator,
     memories: ArrayList(Memory),
     tables: ArrayList(Table),
     globals: ArrayList(u64),
 
-    pub fn init(alloc: *mem.Allocator) Store {
-        var store = Store{
+    pub fn init(alloc: *mem.Allocator) ArrayListStore {
+        var store = ArrayListStore{
             .alloc = alloc,
             .memories = ArrayList(Memory).init(alloc),
             .tables = ArrayList(Table).init(alloc),
@@ -22,19 +31,19 @@ pub const Store = struct {
         return store;
     }
 
-    pub fn addMemory(self: *Store) !*Memory {
+    pub fn addMemory(self: *ArrayListStore) !*Memory {
         const mem_ptr = try self.memories.addOne();
         mem_ptr.* = Memory.init(self.alloc);
         return mem_ptr;
     }
 
-    pub fn addTable(self: *Store, entries: u32, max: ?u32) !*Table {
+    pub fn addTable(self: *ArrayListStore, entries: u32, max: ?u32) !*Table {
         const tbl_ptr = try self.tables.addOne();
         tbl_ptr.* = try Table.init(self.alloc, entries, max);
         return tbl_ptr;
     }
 
-    pub fn addGlobals(self: *Store, count: usize) !void {
+    pub fn addGlobals(self: *ArrayListStore, count: usize) !void {
         _ = try self.globals.resize(count);
         mem.set(u64, self.globals.items[0..], 0);
     }
