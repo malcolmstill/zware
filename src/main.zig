792ad838e38053c97c2099d711a3489c2b6d315e
Malcolm Still <malcolm.still@gmail.com>
2021-02-21 18:18:40 +0000

    feat: export Interpreter and Function so programs can provide host functions

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,8 @@
 pub const Module = @import("module.zig").Module;
 pub const Instance = @import("instance.zig").Instance;
+pub const Interpreter = @import("interpreter.zig").Interpreter;
 pub const Store = @import("store.zig").ArrayListStore;
+pub const Function = @import("function.zig").Function;
 pub const Memory = @import("memory.zig").Memory;
 pub const ValueType = @import("common.zig").ValueType;
 
