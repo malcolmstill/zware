76819d8871a98bec2bb4b6b786766ab276e2121c
Malcolm <malcolm.still@gmail.com>
2021-09-16 21:23:45 +0100

    Paths, paths, paths

--- test/generate.sh
+++ test/generate.sh
@@ -4,78 +4,82 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 TESTSUITE_DIR=$SCRIPT_DIR/testsuite
 GENERATED_DIR=$SCRIPT_DIR/testsuite-generated
 
+pushd $GENERATED_DIR
+
 wast2json --version > $GENERATED_DIR/wast2json.version
 
-wast2json $TESTSUITE_DIR/address.wast -o $GENERATED_DIR/address.json
-wast2json $TESTSUITE_DIR/align.wast -o $GENERATED_DIR/align.json
-wast2json $TESTSUITE_DIR/binary-leb128.wast -o $GENERATED_DIR/binary-leb128.json
-wast2json $TESTSUITE_DIR/binary.wast -o $GENERATED_DIR/binary.json
-wast2json $TESTSUITE_DIR/block.wast -o $GENERATED_DIR/block.json
-wast2json $TESTSUITE_DIR/br_if.wast -o $GENERATED_DIR/br_if.json
-wast2json $TESTSUITE_DIR/br.wast -o $GENERATED_DIR/br.json
-wast2json $TESTSUITE_DIR/br_table.wast -o $GENERATED_DIR/br_table.json
-wast2json $TESTSUITE_DIR/call_indirect.wast -o $GENERATED_DIR/call_indirect.json
-wast2json $TESTSUITE_DIR/call.wast -o $GENERATED_DIR/call.json
-wast2json $TESTSUITE_DIR/comments.wast -o $GENERATED_DIR/comments.json
-wast2json $TESTSUITE_DIR/const.wast -o $GENERATED_DIR/const.json
-wast2json $TESTSUITE_DIR/conversions.wast -o $GENERATED_DIR/conversions.json
-wast2json $TESTSUITE_DIR/custom.wast -o $GENERATED_DIR/custom.json
-wast2json $TESTSUITE_DIR/data.wast -o $GENERATED_DIR/data.json
-wast2json $TESTSUITE_DIR/elem.wast -o $GENERATED_DIR/elem.json
-wast2json $TESTSUITE_DIR/endianness.wast -o $GENERATED_DIR/endianness.json
-wast2json $TESTSUITE_DIR/exports.wast -o $GENERATED_DIR/exports.json
-wast2json $TESTSUITE_DIR/f32_bitwise.wast -o $GENERATED_DIR/f32_bitwise.json
-wast2json $TESTSUITE_DIR/f32_cmp.wast -o $GENERATED_DIR/f32_cmp.json
-wast2json $TESTSUITE_DIR/f32.wast -o $GENERATED_DIR/f32.json
-wast2json $TESTSUITE_DIR/f64_bitwise.wast -o $GENERATED_DIR/f64_bitwise.json
-wast2json $TESTSUITE_DIR/f64_cmp.wast -o $GENERATED_DIR/f64_cmp.json
-wast2json $TESTSUITE_DIR/f64.wast -o $GENERATED_DIR/f64.json
-wast2json $TESTSUITE_DIR/fac.wast -o $GENERATED_DIR/fac.json
-wast2json $TESTSUITE_DIR/float_exprs.wast -o $GENERATED_DIR/float_exprs.json
-wast2json $TESTSUITE_DIR/float_literals.wast -o $GENERATED_DIR/float_literals.json
-wast2json $TESTSUITE_DIR/float_memory.wast -o $GENERATED_DIR/float_memory.json
-wast2json $TESTSUITE_DIR/float_misc.wast -o $GENERATED_DIR/float_misc.json
-wast2json $TESTSUITE_DIR/forward.wast -o $GENERATED_DIR/forward.json
-wast2json $TESTSUITE_DIR/func_ptrs.wast -o $GENERATED_DIR/func_ptrs.json
-wast2json $TESTSUITE_DIR/func.wast -o $GENERATED_DIR/func.json
-wast2json $TESTSUITE_DIR/global.wast -o $GENERATED_DIR/global.json
-wast2json $TESTSUITE_DIR/i32.wast -o $GENERATED_DIR/i32.json
-wast2json $TESTSUITE_DIR/i64.wast -o $GENERATED_DIR/i64.json
-wast2json $TESTSUITE_DIR/if.wast -o $GENERATED_DIR/if.json
-wast2json $TESTSUITE_DIR/imports.wast -o $GENERATED_DIR/imports.json
-wast2json $TESTSUITE_DIR/inline-module.wast -o $GENERATED_DIR/inline-module.json
-wast2json $TESTSUITE_DIR/int_exprs.wast -o $GENERATED_DIR/int_exprs.json
-wast2json $TESTSUITE_DIR/int_literals.wast -o $GENERATED_DIR/int_literals.json
-wast2json $TESTSUITE_DIR/labels.wast -o $GENERATED_DIR/labels.json
-wast2json $TESTSUITE_DIR/left-to-right.wast -o $GENERATED_DIR/left-to-right.json
-wast2json $TESTSUITE_DIR/linking.wast -o $GENERATED_DIR/linking.json
-wast2json $TESTSUITE_DIR/load.wast -o $GENERATED_DIR/load.json
-wast2json $TESTSUITE_DIR/local_get.wast -o $GENERATED_DIR/local_get.json
-wast2json $TESTSUITE_DIR/local_set.wast -o $GENERATED_DIR/local_set.json
-wast2json $TESTSUITE_DIR/local_tee.wast -o $GENERATED_DIR/local_tee.json
-wast2json $TESTSUITE_DIR/loop.wast -o $GENERATED_DIR/loop.json
-wast2json $TESTSUITE_DIR/memory_grow.wast -o $GENERATED_DIR/memory_grow.json
-wast2json $TESTSUITE_DIR/memory_redundancy.wast -o $GENERATED_DIR/memory_redundancy.json
-wast2json $TESTSUITE_DIR/memory_size.wast -o $GENERATED_DIR/memory_size.json
-wast2json $TESTSUITE_DIR/memory_trap.wast -o $GENERATED_DIR/memory_trap.json
-wast2json $TESTSUITE_DIR/memory.wast -o $GENERATED_DIR/memory.json
-wast2json $TESTSUITE_DIR/names.wast -o $GENERATED_DIR/names.json
-wast2json $TESTSUITE_DIR/nop.wast -o $GENERATED_DIR/nop.json
-wast2json $TESTSUITE_DIR/return.wast -o $GENERATED_DIR/return.json
-wast2json $TESTSUITE_DIR/select.wast -o $GENERATED_DIR/select.json
-wast2json $TESTSUITE_DIR/skip-stack-guard-page.wast -o $GENERATED_DIR/skip-stack-guard-page.json
-wast2json $TESTSUITE_DIR/stack.wast -o $GENERATED_DIR/stack.json
-wast2json $TESTSUITE_DIR/start.wast -o $GENERATED_DIR/start.json
-wast2json $TESTSUITE_DIR/store.wast -o $GENERATED_DIR/store.json
-wast2json $TESTSUITE_DIR/switch.wast -o $GENERATED_DIR/switch.json
-wast2json $TESTSUITE_DIR/table.wast -o $GENERATED_DIR/table.json
-wast2json $TESTSUITE_DIR/token.wast -o $GENERATED_DIR/token.json
-wast2json $TESTSUITE_DIR/traps.wast -o $GENERATED_DIR/traps.json
-wast2json $TESTSUITE_DIR/type.wast -o $GENERATED_DIR/type.json
-wast2json $TESTSUITE_DIR/unreachable.wast -o $GENERATED_DIR/unreachable.json
-wast2json $TESTSUITE_DIR/unreached-invalid.wast -o $GENERATED_DIR/unreached-invalid.json
-wast2json $TESTSUITE_DIR/unwind.wast -o $GENERATED_DIR/unwind.json
-wast2json $TESTSUITE_DIR/utf8-custom-section-id.wast -o $GENERATED_DIR/utf8-custom-section-id.json
-wast2json $TESTSUITE_DIR/utf8-import-field.wast -o $GENERATED_DIR/utf8-import-field.json
-wast2json $TESTSUITE_DIR/utf8-import-module.wast -o $GENERATED_DIR/utf8-import-module.json
-wast2json $TESTSUITE_DIR/utf8-invalid-encoding.wast -o $GENERATED_DIR/utf8-invalid-encoding.json
+wast2json $TESTSUITE_DIR/address.wast
+wast2json $TESTSUITE_DIR/align.wast
+wast2json $TESTSUITE_DIR/binary-leb128.wast
+wast2json $TESTSUITE_DIR/binary.wast
+wast2json $TESTSUITE_DIR/block.wast
+wast2json $TESTSUITE_DIR/br_if.wast
+wast2json $TESTSUITE_DIR/br.wast
+wast2json $TESTSUITE_DIR/br_table.wast
+wast2json $TESTSUITE_DIR/call_indirect.wast
+wast2json $TESTSUITE_DIR/call.wast
+wast2json $TESTSUITE_DIR/comments.wast
+wast2json $TESTSUITE_DIR/const.wast
+wast2json $TESTSUITE_DIR/conversions.wast
+wast2json $TESTSUITE_DIR/custom.wast
+wast2json $TESTSUITE_DIR/data.wast
+wast2json $TESTSUITE_DIR/elem.wast
+wast2json $TESTSUITE_DIR/endianness.wast
+wast2json $TESTSUITE_DIR/exports.wast
+wast2json $TESTSUITE_DIR/f32_bitwise.wast
+wast2json $TESTSUITE_DIR/f32_cmp.wast
+wast2json $TESTSUITE_DIR/f32.wast
+wast2json $TESTSUITE_DIR/f64_bitwise.wast
+wast2json $TESTSUITE_DIR/f64_cmp.wast
+wast2json $TESTSUITE_DIR/f64.wast
+wast2json $TESTSUITE_DIR/fac.wast
+wast2json $TESTSUITE_DIR/float_exprs.wast
+wast2json $TESTSUITE_DIR/float_literals.wast
+wast2json $TESTSUITE_DIR/float_memory.wast
+wast2json $TESTSUITE_DIR/float_misc.wast
+wast2json $TESTSUITE_DIR/forward.wast
+wast2json $TESTSUITE_DIR/func_ptrs.wast
+wast2json $TESTSUITE_DIR/func.wast
+wast2json $TESTSUITE_DIR/global.wast
+wast2json $TESTSUITE_DIR/i32.wast
+wast2json $TESTSUITE_DIR/i64.wast
+wast2json $TESTSUITE_DIR/if.wast
+wast2json $TESTSUITE_DIR/imports.wast
+wast2json $TESTSUITE_DIR/inline-module.wast
+wast2json $TESTSUITE_DIR/int_exprs.wast
+wast2json $TESTSUITE_DIR/int_literals.wast
+wast2json $TESTSUITE_DIR/labels.wast
+wast2json $TESTSUITE_DIR/left-to-right.wast
+wast2json $TESTSUITE_DIR/linking.wast
+wast2json $TESTSUITE_DIR/load.wast
+wast2json $TESTSUITE_DIR/local_get.wast
+wast2json $TESTSUITE_DIR/local_set.wast
+wast2json $TESTSUITE_DIR/local_tee.wast
+wast2json $TESTSUITE_DIR/loop.wast
+wast2json $TESTSUITE_DIR/memory_grow.wast
+wast2json $TESTSUITE_DIR/memory_redundancy.wast
+wast2json $TESTSUITE_DIR/memory_size.wast
+wast2json $TESTSUITE_DIR/memory_trap.wast
+wast2json $TESTSUITE_DIR/memory.wast
+wast2json $TESTSUITE_DIR/names.wast
+wast2json $TESTSUITE_DIR/nop.wast
+wast2json $TESTSUITE_DIR/return.wast
+wast2json $TESTSUITE_DIR/select.wast
+wast2json $TESTSUITE_DIR/skip-stack-guard-page.wast
+wast2json $TESTSUITE_DIR/stack.wast
+wast2json $TESTSUITE_DIR/start.wast
+wast2json $TESTSUITE_DIR/store.wast
+wast2json $TESTSUITE_DIR/switch.wast
+wast2json $TESTSUITE_DIR/table.wast
+wast2json $TESTSUITE_DIR/token.wast
+wast2json $TESTSUITE_DIR/traps.wast
+wast2json $TESTSUITE_DIR/type.wast
+wast2json $TESTSUITE_DIR/unreachable.wast
+wast2json $TESTSUITE_DIR/unreached-invalid.wast
+wast2json $TESTSUITE_DIR/unwind.wast
+wast2json $TESTSUITE_DIR/utf8-custom-section-id.wast
+wast2json $TESTSUITE_DIR/utf8-import-field.wast
+wast2json $TESTSUITE_DIR/utf8-import-module.wast
+wast2json $TESTSUITE_DIR/utf8-invalid-encoding.wast
+
+popd
