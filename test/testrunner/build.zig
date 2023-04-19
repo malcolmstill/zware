dfe64aab9ed5cf5155c5c67c203ca54fba7b26dd
radar roark <122068506+radarroark@users.noreply.github.com>
2023-04-19 08:33:44 -0400

    update build scripts

--- test/testrunner/build.zig
+++ test/testrunner/build.zig
@@ -1,23 +1,21 @@
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const exe = b.addExecutable(.{
+        .name = "testrunner",
+        .root_source_file = .{ .path = "src/testrunner.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.addAnonymousModule("zware", .{
+        .source_file = .{ .path = "../../src/main.zig" },
+    });
+    b.installArtifact(exe);
 
-    const exe = b.addExecutable("testrunner", "src/testrunner.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.addPackagePath("zware", "../../src/main.zig");
-    exe.install();
-
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
