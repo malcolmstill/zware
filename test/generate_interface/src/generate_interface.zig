006d4ddc47663df6ab5f6d5c9a34cc1e709a929f
Malcolm Still <malcolm.still@gmail.com>
2023-07-09 19:21:20 +0100

    Generate wrappers for invoking wasm functions

--- test/generate_interface/src/generate_interface.zig
+++ test/generate_interface/src/generate_interface.zig
@@ -96,5 +96,75 @@ pub fn main() !void {
         try stdout.print("}}\n\n", .{});
     }
 
+    // Generate stubs
+    for (module.exports.list.items) |exprt| {
+        if (exprt.tag != .Func) continue;
+
+        const function = module.functions.list.items[exprt.index];
+
+        const function_type = module.types.list.items[function.typeidx];
+
+        // Emit function definition
+        try stdout.print("pub fn {s}(instance: *zware.Instance", .{exprt.name}); 
+
+        // Emit function params
+        for (function_type.params, 0..) |_, i| {
+            try stdout.print(", param{}: u64", .{i});
+        }
+
+        try stdout.print(") zware.WasmError!", .{});
+
+        if (function_type.results.len == 0) {
+            try stdout.print("void", .{});
+        } else if (function_type.results.len == 1) {
+            try stdout.print("u64", .{});
+        } else {
+            try stdout.print("struct {{", .{});
+
+            for (function_type.results, 0..) |_, i| {
+                try stdout.print("result{}: u64,\n", .{i});
+            }
+
+            try stdout.print("}}", .{});
+        }
+
+        try stdout.print(" {{\n", .{});
+
+        // Push params into input array
+        try stdout.print("\tvar in = [_]u64{{", .{});
+        for (function_type.params, 0..) |_, i| {
+            try stdout.print("param{}", .{i});
+            if (i < function_type.params.len - 1) try stdout.print(", ", .{});
+        }
+        try stdout.print("}};\n", .{});
+
+
+        try stdout.print("\tvar out = [_]u64{{", .{});
+        for (function_type.results, 0..) |_, i| {
+            try stdout.print("0", .{});
+            if (i < function_type.results.len - 1) try stdout.print(", ", .{});
+        }        
+        try stdout.print("}};\n", .{});
+
+        try stdout.print("\ttry instance.invoke(\"{s}\", in[0..], out[0..], .{{}});\n", .{exprt.name});
+
+        // Return results
+        if (function_type.results.len == 0) {
+            //
+        } else if (function_type.results.len == 1) {
+            try stdout.print("\treturn out[0];\n", .{});
+        } else {
+            try stdout.print("\treturn .{{", .{});
+
+            for (function_type.results, 0..) |_, i| {
+                try stdout.print(".result{} = out[{}],\n", .{i, i});
+            }
+
+            try stdout.print("}};\n", .{});
+        }
+
+        try stdout.print("}}\n\n", .{});
+    }
+
     try bw.flush();
 }
